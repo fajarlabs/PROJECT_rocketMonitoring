bbb5965e6fc5181b4ee93fb5eed163a4c0cec7f2
fajarrdp <fajarrdp@gmail.com>
2021-09-18 17:29:26 +0700

    change folder

---
+++ MICROCONTROLLER/rocket_receiver/rocket_receiver.ino
@@ -0,0 +1,28 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("<<MSG:The receiver is running>>");
+
+  if (!LoRa.begin(915E6)) {
+    Serial.println("<<ERR:Starting LoRa failed>>");
+    while (1);
+  }
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+    Serial.print("<<RSSI:");
+    Serial.print(LoRa.packetRssi());
+    Serial.print(">>");
+    Serial.println();
+  }
+}
